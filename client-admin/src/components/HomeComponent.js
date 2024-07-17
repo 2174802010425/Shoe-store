55ffd5e76ca05672663136bcd9bdc01e7db1750f
hoanhon03 <113343821+hoanhon03@users.noreply.github.com>
2024-07-17 22:10:31 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN TRANG CHỦ</h2>
+        <img src="https://img.upanh.tv/2024/07/13/57615b8c0092a66c1d4058b1692955cc.gif" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
