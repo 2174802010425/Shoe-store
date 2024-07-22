f6fb0c563e06de896b978cb28b3d9f046d717b6f
hoanhontran <113343821+hoanhon03@users.noreply.github.com>
2024-07-22 11:36:04 +0700

    Update LoginComponent.js

--- client-admin/src/components/LoginComponent.js
+++ client-admin/src/components/LoginComponent.js
@@ -14,6 +14,7 @@ class Login extends Component {
   render() {
     if (this.context.token === '') {
       return (
+        <div classname="login-container>
         <div className="align-valign-center">
           <h2 className="text-center">ADMIN LOGIN</h2>
           <form>
@@ -34,6 +35,7 @@ class Login extends Component {
               </tbody>
             </table>
           </form>
+        </div>
         </div>
       );
     }
