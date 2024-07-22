53d05d9fa64db46481696d0d31f42c1076164848
hoanhontran <113343821+hoanhon03@users.noreply.github.com>
2024-07-22 11:29:05 +0700

    Update HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -5,8 +5,10 @@ class Home extends Component {
     return (
       <div className="align-center">
         <h2 className="text-center">ADMIN TRANG CHỦ</h2>
+       <div classname="img-contanner">
         <img src="https://img.upanh.tv/2024/07/22/LgnH.gif" 
         width="800px" height="600px" alt="" />
+          </div>
      
       </div>
     );
