014ef2e4eec2aa71e62d2cdba2396cb8c60c8cf2
hoanhontran <113343821+hoanhon03@users.noreply.github.com>
2024-07-22 11:51:34 +0700

    Update LoginComponent.js

--- client-admin/src/components/LoginComponent.js
+++ client-admin/src/components/LoginComponent.js
@@ -1,43 +1,45 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import MyContext from '../contexts/MyContext';
-import withRouter from '../utils/withRouter';
 
 class Login extends Component {
   static contextType = MyContext; // using this.context to access global state
   constructor(props) {
     super(props);
     this.state = {
-      txtUsername: 'customer1',
-      txtPassword: '123467'
+      txtUsername: '',
+      txtPassword: ''
     };
   }
   render() {
-    return (
-      <div className='login-container'>
-        <div className="align-center">
-        <h2 className="text-center">CUSTOMER LOGIN</h2>
-        <form>
-          <table className="align-center">
-            <tbody>
-              <tr>
-                <td>Username</td>
-                <td><input type="text" value={this.state.txtUsername} onChange={(e) => { this.setState({ txtUsername: e.target.value }) }} /></td>
-              </tr>
-              <tr>
-                <td>Password</td>
-                <td><input type="password" value={this.state.txtPassword} onChange={(e) => { this.setState({ txtPassword: e.target.value }) }} /></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td><input type="submit" value="LOGIN" onClick={(e) => this.btnLoginClick(e)} /></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
-      </div>
-    );
+    if (this.context.token === '') {
+      return (
+        <div className='login-container'>
+          <div className="align-valign-center">
+            <h2 className="text-center">ADMIN LOGIN</h2>
+            <form>
+              <table className="align-center">
+                <tbody>
+                  <tr>
+                    <td>Username</td>
+                    <td><input type="text" value={this.state.txtUsername} onChange={(e) => { this.setState({ txtUsername: e.target.value }) }} /></td>
+                  </tr>
+                  <tr>
+                    <td>Password</td>
+                    <td><input type="password" value={this.state.txtPassword} onChange={(e) => { this.setState({ txtPassword: e.target.value }) }} /></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td><input type="submit" value="LOGIN" onClick={(e) => this.btnLoginClick(e)} /></td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </div>
+        </div>
+      );
+    }
+    return (<div />);
   }
   // event-handlers
   btnLoginClick(e) {
@@ -53,16 +55,15 @@ class Login extends Component {
   }
   // apis
   apiLogin(account) {
-    axios.post('/api/customer/login', account).then((res) => {
+    axios.post('/api/admin/login', account).then((res) => {
       const result = res.data;
       if (result.success === true) {
         this.context.setToken(result.token);
-        this.context.setCustomer(result.customer);
-        this.props.navigate('/home');
+        this.context.setUsername(account.username);
       } else {
         alert(result.message);
       }
     });
   }
 }
-export default withRouter(Login);
+export default Login;
