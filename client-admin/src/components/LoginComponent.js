aed1e85aa021f3ce6d386cb85dd0e253c49d1748
hoanhontran <113343821+hoanhon03@users.noreply.github.com>
2024-07-22 11:44:45 +0700

    Update LoginComponent.js

--- client-admin/src/components/LoginComponent.js
+++ client-admin/src/components/LoginComponent.js
@@ -1,4 +1,21 @@
-<div className="align-center">
+import axios from 'axios';
+import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
+import withRouter from '../utils/withRouter';
+
+class Login extends Component {
+  static contextType = MyContext; // using this.context to access global state
+  constructor(props) {
+    super(props);
+    this.state = {
+      txtUsername: 'customer1',
+      txtPassword: '123467'
+    };
+  }
+  render() {
+    return (
+      <div className='login-container'>
+        <div className="align-center">
         <h2 className="text-center">CUSTOMER LOGIN</h2>
         <form>
           <table className="align-center">
@@ -19,3 +36,33 @@
           </table>
         </form>
       </div>
+      </div>
+    );
+  }
+  // event-handlers
+  btnLoginClick(e) {
+    e.preventDefault();
+    const username = this.state.txtUsername;
+    const password = this.state.txtPassword;
+    if (username && password) {
+      const account = { username: username, password: password };
+      this.apiLogin(account);
+    } else {
+      alert('Please input username and password');
+    }
+  }
+  // apis
+  apiLogin(account) {
+    axios.post('/api/customer/login', account).then((res) => {
+      const result = res.data;
+      if (result.success === true) {
+        this.context.setToken(result.token);
+        this.context.setCustomer(result.customer);
+        this.props.navigate('/home');
+      } else {
+        alert(result.message);
+      }
+    });
+  }
+}
+export default withRouter(Login);
